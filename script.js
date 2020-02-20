fb13117f4ece4ab6c8b966f6ef8ad006d71f5ff3
causingchaos <none>
2020-02-20 01:01:21 -0700

    JS login code mostly done

--- script.js
+++ script.js
@@ -6,19 +6,84 @@ class AppForm {
 
     this.setListeners();
     this.getForm();
+    document.getElementById('next-button').disabled = true;
     this.refresh(); //incremenet step from 0 to 1, and update 'none' to 'block'
+    this.check();
+    this.pageType = window.location.href.includes('login') ? 'login' : 'signup';
   }
 
   refresh = () => {
     this.step++;
     //console.log("step: "+ this.step);
-    if (this.step <= this.form.length) //check if were on last step in the form
+    if (this.step <= this.form.length){ //check if were on last step in the form
       this.displayStep();
+      this.removeListeners();
+      document.getElementById('next-button').disabled = true;
+      this.check();
+    }
     else
       this.submit();
   }
 
+  goBack = () => {
+    if(this.step > 1) {
+      this.currentInput().value = '';
+      this.step--;
+      this.displayStep();
+      this.enableDisable();
+    }
+  }
+
   submit = () => console.log('SUBMIT');
+
+  currentInput = () => this.form[this.step-1].input;
+  previousInput = () => this.form[this.step-2].input;
+
+  check = () => this.currentInput().addEventListener('keyup', this.enableDisable);
+
+  enableDisable = () => {
+    if(this.valid(this.currentInput())) {
+      this.currentInput().classList.remove('invalid');
+      this.setListeners();
+      document.getElementById('next-button').disabled = false;
+    }else{
+      this.currentInput().classList.add('invalid');
+      this.removeListeners();
+      document.getElementById('next-button').disabled = true;
+    }
+  }
+
+  valid = (input) => {
+    const formType = input.id;
+    console.log(formType);
+    const value = input.value; 
+    console.log(value); 
+    //check for whitespace, return true if it's empty, false if whitespace found
+    const empty = (str) => !str.split('').every(_char => _char !== ' ');
+    console.log(value) 
+
+    if( !value || empty(value) ) return false;
+                                     //value exists and is not whitespaces
+    if(this.pageType === 'login') return value && !empty(value); //doesnt need full address on login page
+
+    switch(formType) {
+      case 'email-input':
+        return /\S+@\S+\.\S+/.test(value) //test for valid email
+
+      case 'email-verification-input':
+        return this.previousInput().value === value;
+
+      case 'password-input':
+        return /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9@$&!]{8,}/.test(value)
+
+      // ( one digit ? )(one lower case)(1 upper case)(can contain symbols)(at least 8 long)
+      case 'password-verification-input':
+        return this.previousInput().value === value;
+
+      default:
+        return false;         
+    }
+  };
 
   displayStep = () => {  
     if(this.currentGroup) //If we have a current group, access style and hide it
@@ -47,6 +112,7 @@ class AppForm {
 
   setListeners = () => {
     document.getElementById('next-button').addEventListener('click', this.refresh);
+    document.getElementById('back-button').addEventListener('click', this.goBack);
   }
 
   removeListeners = () => {
